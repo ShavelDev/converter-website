82cb5ab90c492c83cc218ed86cb9b4eefb273966
ShavelDev <nor.szaw@gmail.com>
2024-08-20 18:33:06 +0200

    protorype html finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,51 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [file, changeFile] = useState<File | null>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(!file){
+      console.error("No file provided");
+    }
+
+    
+
+  }
+
   return (
     <>
       <div className="header">
         <h1> <strong>Image Converter</strong>  </h1>
       </div>
       <main className="main">
+        
+       
+      <Image
+        className="comparison-image"
+        src={"/image_comparision.png"}
+        alt="Comparison"
+        width={774}
+        height={400}
+      />
 
+      <form onSubmit={submitHandler}>
+    
+        <input type="file"
+        required />
+
+        <button type="submit"> Submit</button>
+
+
+
+      </form>
+
+          
+       
 
       </main>
       <footer className="footer-text">
@@ -15,9 +53,6 @@ export default function Home() {
         ShavelDev
         </p>
       </footer>
-
-
-
 
     </>
   );
